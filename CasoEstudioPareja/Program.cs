7823a1d96862f6e3baec23ae7e91d3b1df9ee0cc
ClaudiaLiraB <clucialirab@uamv.edu.ni>
2025-11-23 19:18:52 -0600

    Clase libro

--- CasoEstudioPareja/Program.cs
+++ CasoEstudioPareja/Program.cs
@@ -6,6 +6,27 @@ using System.Threading.Tasks;
 
 namespace CasoEstudioPareja
 {
+
+    // Clase para representar un libro
+    internal class Libro
+    {
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Año { get; set; }
+        public string Descripcion { get; set; }
+        public Libro(string titulo, string autor, int ano, string descripcion)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            Año = ano;
+            Descripcion = descripcion;
+        }
+        public override string ToString()
+        {
+            return $"Título: {Titulo}, Autor: {Autor}, Año: {Año}, Descripción: {Descripcion}";
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
